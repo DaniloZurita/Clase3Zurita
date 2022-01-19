dc08961b771f0e04d15cb029e28a15d493f5f4db
Danilo Zurita <danizurita4@gmail.com>
2022-01-19 02:53:44 -0300

    complementario

---
+++ main.js
@@ -0,0 +1,7 @@
+let venta = prompt("Queres comprar algo?")
+while (venta != "No"){
+    
+    alert("Buenisimo mira todos nuestros productos");
+    alert(venta);
+    venta = prompt("Si no te interesa comprar responde No")
+}
